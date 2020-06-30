702732f4abf17303f1e772a3e7c6a95526ed2d6c
EmiliaKruszewska <emikruszewskaa@gmail.com>
2020-06-30 14:38:39 +0200

    navigation

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 const burgerIcon = document.querySelector('.burger');
+const topMenu = document.querySelector('.top-menu');
+const backgroundMenuColor = document.querySelector('.bgc');
 
 burgerIcon.addEventListener('click', function(){
-
     burgerIcon.classList.toggle('active');
+    topMenu.classList.toggle('active');
+    backgroundMenuColor.classList.toggle('active');
 })
